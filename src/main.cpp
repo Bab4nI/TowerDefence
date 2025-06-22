dceea0a78aa6c8bc96720f741cbce1b59db7c08f
bab4n <blackbigbagb@gmail.com>
2025-06-22 21:58:04 +0300

    The defeat screen is absolutely transparent. The problem with castle's hp text has solved

--- src/main.cpp
+++ src/main.cpp
@@ -53,37 +53,36 @@ int main()
     castle_hp_text.setFont(castle_hp_font);
     castle_hp_text.setCharacterSize(20);
     castle_hp_text.setFillColor(sf::Color::Red);
-
-    sf::FloatRect textRect = castle_hp_text.getLocalBounds();
-    castle_hp_text.setOrigin(textRect.width, textRect.height); 
-    castle_hp_text.setPosition(window.getSize().x - 10.f, window.getSize().y - 10.f);
+ 
     int cur_castle_hp = level1.castle.get_health();
     std::string castle_hp_string = std::to_string(cur_castle_hp) + " HP";
-
     castle_hp_text.setString(castle_hp_string);
+    sf::FloatRect textRect = castle_hp_text.getLocalBounds();
+        castle_hp_text.setOrigin(textRect.left + textRect.width,
+                                textRect.top  + textRect.height);
+    castle_hp_text.setPosition(window.getSize().x - 15.0f, window.getSize().y - 15.0f);
 
     sf::Texture defeat_screen;
     if(!defeat_screen.loadFromFile("assets\\images\\defeat_screen.png"))
         return EXIT_FAILURE;
     
     sf::Sprite defeat_sprite = sf::Sprite(defeat_screen);
-
+    defeat_sprite.setColor(sf::Color(50, 0, 0, 128)); // 128 -> ~50% opacity
+    
     while (window.isOpen())
     {
-        if(!castle_is_destroyed){
-            sf::Event event;
-            while (window.pollEvent(event))
-            {
-                if (event.type == sf::Event::Closed)
-                    window.close();
-                else if (event.type == sf::Event::Resized){
-                    window_size = window.getSize();
-                    window_size.x = window_size.y * coef;
-                    window.setSize(window_size);
-                    castle_hp_text.setPosition(window.getSize().x - 10.f, window.getSize().y - 10.f);
-                }
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+            else if (event.type == sf::Event::Resized){
+                window_size = window.getSize();
+                window_size.x = window_size.y * coef;
+                window.setSize(window_size);
             }
-
+        }
+        if(!castle_is_destroyed){
             if (spawn_clock.getElapsedTime().asSeconds() >= spawn_interval){
                 enemies.push_back(new Enemies::Goblin());
                 spawn_clock.restart();
@@ -99,6 +98,9 @@ int main()
         cur_castle_hp = level1.castle.get_health();
         castle_hp_string = std::to_string(cur_castle_hp) + " HP";
         castle_hp_text.setString(castle_hp_string);
+        sf::FloatRect textRect = castle_hp_text.getLocalBounds();
+        castle_hp_text.setOrigin(textRect.left + textRect.width,
+                                textRect.top  + textRect.height);
 
         if(castle_is_destroyed){
             window.draw(defeat_sprite);
